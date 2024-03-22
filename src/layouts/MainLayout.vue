475bcb2467fe5571a7a4e79be3ce99c6c8f4e668
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-03-22 14:28:39 +0200

    initial setup

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,86 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title> Task Manager </q-toolbar-title>
+
+        <div>v{{ appVersion }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header> Essential Links </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
+
+const linksList = [
+  {
+    title: "Home",
+    icon: "home",
+    link: "https://quasar.dev",
+  },
+  {
+    title: "Todos",
+    icon: "list",
+    link: "https://github.com/quasarframework",
+  },
+  {
+    title: "New Todo",
+    icon: "task",
+    link: "https://chat.quasar.dev",
+  },
+  {
+    title: "Settings",
+    icon: "settings",
+    link: "https://chat.quasar.dev",
+  },
+];
+
+export default defineComponent({
+  name: "MainLayout",
+
+  components: {
+    EssentialLink,
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+    const appVersion = "1.0.0";
+
+    return {
+      appVersion,
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="column">
+    <div class="row">
+      <q-btn  label="New Task" class="q-my-md q-mx-lg" />
+    </div>
+    <div >
+      <h2 class="text-center">Tasks</h2>
+      <div class="q-pa-md">
+    <q-list>
+      <q-item>
+        <q-item-section>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption lines="2">Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>5 min left</q-item-label>
+          <div class="row text-h6">
+            <q-icon name="check" color="blue" />
+            <q-icon name="delete" color="red" />
+          </div>
+        </q-item-section>
+      </q-item>
+
+      <q-separator spaced inset />
+      
+      <q-item>
+        <q-item-section>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption lines="2">Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>5 min left</q-item-label>
+          <div class="row text-h6">
+            <q-icon name="check" color="blue" />
+            <q-icon name="delete" color="red" />
+          </div>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </div>
+    </div>
+  </div>
+</template>
+
+<script></script>
